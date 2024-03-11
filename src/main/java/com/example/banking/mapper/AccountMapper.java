e5563df91bebf1570e08f31dcf81ff6b4d3c15ed
Nagendra-Sure <nagendrasure111@gmail.com>
2024-03-11 19:25:14 +0530

    RowMapper changes

--- src/main/java/com/example/banking/mapper/AccountMapper.java
+++ src/main/java/com/example/banking/mapper/AccountMapper.java
@@ -24,6 +24,7 @@ public class AccountMapper {
                 account.getId(),
                 account.getAccountHolderName(),
                 account.getBalance()
+
         );
 
         return accountDto;
